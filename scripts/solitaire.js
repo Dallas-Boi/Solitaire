422b5d87eaee5f25456a017cbe6c7d39317f5e5b
Dallas-Boi <lildallas463@gmail.com>
2023-12-04 08:58:31 -0500

    Updating Backend for Blackjack to be implemented

--- scripts/solitaire.js
+++ scripts/solitaire.js
@@ -1,16 +1,9 @@
 // Made Friday, November 17th, 2023
 // Main Values
-const card_deck = [
-	"AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
-	"AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
-	"AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
-	"AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
-]
 const divChange_event = [] // This will hold all observe events for set_top
 const draw_cards = document.getElementById("draw_cards")
 const drawed_cards = document.getElementById("drawed_cards")
 const drag_cards = document.getElementById("dragging_items")
-const check_Overlap = (elm1, elm2) => (elm1.right < elm2.left || elm1.left > elm2.right || elm1.bottom < elm2.top || elm1.top > elm2.bottom)
 var draw_amount = 1
 var cur_drag = null
 var old_cont = null
@@ -19,29 +12,14 @@ var select_elm = 0
 // The aniamation time line
 var timeline = anime.timeline();
 
-// Shuffles the given array
-function shuffle(array) {
-	for (let i = array.length - 1; i > 0; i--) {
-		let j = Math.floor(Math.random() * i);
-		let temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-}
-
 // Converts the cards Letter to a Number
-function convertLetter_toNumber(letter) {
-	if (letter == "A") { return 1 }
-	else if (letter == "J") { return 11 }
-	else if (letter == "Q") { return 12 }
-	else if (letter == "K") { return 13 }
-	else { return letter }
-}
-
-// Returns the card set color
-function getCardSetColor(card_set) {
-	if (["D", "H"].includes(card_set)) { return "red" }
-	else if (["C", "S"].includes(card_set)) { return "black" }
+function convertLetter_toNumber_solitaire(letter) {
+	if (letter[0] == "A") { return 1 }
+	else if (letter[0] == "J") { return 11 }
+	else if (letter[0] == "Q") { return 12 }
+	else if (letter[0] == "K") { return 13 }
+	else if (letter[1] == "0") { return 10 }
+	else { return letter[0] }
 }
 
 // Handles the winning animations
@@ -67,7 +45,7 @@ function check_set_card(elm, set, rtn) {
 	if (set.id.includes("set")) {elm_data = set}
 	// If the player moves a card into the finished cards elements
 	if (elm_data.className.includes("finish_card")) {
-		if ((elm_data.childElementCount == convertLetter_toNumber(elm.id[0])) && (elm_data.firstChild.id[1].toUpperCase() == elm.id[1])) {move_card(elm.id, elm_data.id)} // If the cards number is equaled to the amount of elements in that container
+		if ((elm_data.childElementCount == convertLetter_toNumber_solitaire(elm.id[0])) && (elm_data.firstChild.id[1].toUpperCase() == elm.id[1])) {move_card(elm.id, elm_data.id)} // If the cards number is equaled to the amount of elements in that container
 		else {move_card(elm.id, old_cont.id)} // Moves the card to its origial Placement
 		return
 	}
@@ -77,14 +55,12 @@ function check_set_card(elm, set, rtn) {
 	}
 	// A list of the checking data
 	var check_data = [
-		parseInt(convertLetter_toNumber(elm.id[0])),
-		parseInt(convertLetter_toNumber(elm_data.lastChild.id[0])),
+		parseInt(convertLetter_toNumber_solitaire(elm.id)),
+		parseInt(convertLetter_toNumber_solitaire(elm_data.lastChild.id)),
 		getCardSetColor(elm.id[1]),
 		getCardSetColor(elm_data.lastChild.id[1])
 	]
-	// Fixes the issue with 10 being used as 1
-	if (elm.id[1] == "0") { check_data[0] = 10 }
-	if (elm_data.lastChild.id[1] == "0") { check_data[1] = 10 }
+
 	// Checks if the numbers are in order and if they are not the same color
 	if ((check_data[0] == check_data[1] - 1) && (check_data[2] !== check_data[3])) {
 		if (rtn !== true) {move_card(elm.id, elm_data.id)}
@@ -138,46 +114,18 @@ window.onload = function () {
 }
 
 // Places the Card
-function make_card(card, set, clss) {
-	// Sets the card Suit
-	var suit;
-	var cardClass = clss
-	if (card[card.length-1] == "H") {suit = "♥️"}
-	else if (card[card.length-1] == "S") {suit = "♠️"}
-	else if (card[card.length-1] == "D") {suit = "♦️"}
-	else if (card[card.length-1] == "C") {suit = "♣️"}
-	if (!(clss)) {cardClass = "card"}
-	// Makes the card
-	var thisCard = document.createElement("div") // The card
-	thisCard.id=card
-	thisCard.className = cardClass
-	var cardNum = document.createElement("div") // The Card Number
-	cardNum.className = `card_num color_${getCardSetColor(card[card.length-1])}`
-	cardNum.textContent = card[0]
-	if (card[1] == "0") {cardNum.textContent = "10"}
-	var cardSuitT = document.createElement("div") // The Card Suit Top
-	cardSuitT.className = `card_suit_top color_${getCardSetColor(card[card.length-1])}`
-	cardSuitT.textContent = suit
-	var cardSuitB = document.createElement("div") // The Card Suit Bottom
-	cardSuitB.className = `card_suit_bottom color_${getCardSetColor(card[card.length-1])}`
-	cardSuitB.textContent = suit
-	// Adds it to the card
-	thisCard.appendChild(cardNum)
-	thisCard.appendChild(cardSuitT)
-	thisCard.appendChild(cardSuitB)
-	// Card is added to the board
-	draw_cards.appendChild(thisCard)
-	card_deck.splice(0, 1) // Removes the first card in the deck
+function place_card() {
+	
 }
 
 // Starts the Game 
 function start_game() {
-	shuffle(card_deck) // Ranomizes the Cards
 	// Places the cards in the draw_container
 	for (var crd = card_deck.length; crd > 0; crd--) { 
-		make_card(card_deck[0], `draw_cards`, "card card_hidden") 
+		// Card is added to the board
+		console.log(card_deck[crd-1])
+		draw_cards.appendChild(make_card(card_deck[crd-1], "card card_hidden"))
 	}
-	console.log(card_deck)
 	// Moves the element to the board
 	for (var set=1; set < 8; set++) {
 		for (var crd=0; crd < set; crd++) {
