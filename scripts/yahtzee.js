424099e7519219fc74767d653fa57da9d6c849b0
Dallas-Boi <lildallas463@gmail.com>
2023-12-13 12:22:45 -0500

    Allowed players to insert their inputs

--- scripts/yahtzee.js
+++ scripts/yahtzee.js
@@ -1,20 +1,15 @@
 // Made Friday, December 8th, 2023
 // Game variables
-var currTurn = 1
+var currTurn = 0
 var rolls = 0
 var diceVal = []
 var dieList = [] // All the dice classes
-// Makes the player
-class Player {
-    constructor(id, points, data) {
-        this.id = id
-        this.points = points
-        this.data = data
-    }
-}
+var players = {"player1":{"total":0, "all_total": 0}, "player2":{"total":0, "all_total": 0}}
+// All paper items
+var items = ["ones", "twos", "threes", "fours", "fives", "sixes"]
+var otherItm = [...items, "three_kind", "four_kind", "house", "sm_straight", "lar_straight", "chance", "yahtzee"]
 // Makes the dice
 class Dice {
-
     constructor(dice) {
         this.dice = dice
         this.saved = false
@@ -27,32 +22,28 @@ class Dice {
     get_dice() { return this.dice }
     get_dice_num() {return this.dice_num}
     isSaved() {return this.saved}
-    // saves the dice
-    save_die() {
-        this.saved = true
+    // Moves the dice
+    move_die(x, y, rotate) {
         anime({
             targets: this.dice,
-            translateX: 0,
-            translateY: 0,
-            rotate: 0,
+            translateX: x,
+            translateY: y,
+            rotate: rotate,
             easing: 'easeInOutQuad',
             duration: 100
         })
+    }
+    // saves the dice
+    save_die() {
+        this.saved = true
+        this.move_die(0, 0, 0)
     }
     // Unsaves the dice
     unsave_die() {
         this.saved = false
         // Brings the die back to the its original position
-        anime({
-            targets: this.dice,
-            translateX: this.x,
-            translateY: this.y,
-            rotate: this.rotate,
-            easing: 'easeInOutQuad',
-            duration: 100
-        })
+        this.move_die(this.x, this.y, this.rotate)
     }
-
     // When the player rolls the dice
     roll_dice(num) {
         this.dice_num = num
@@ -78,17 +69,82 @@ class Dice {
         this.y = anime.random(-100, -400)
         this.rotate = anime.random(0, 360)
         // Animates the Elements movement
-        anime({
-            targets: this.dice,
-            translateX: this.x,
-            translateY: this.y,
-            rotate: this.rotate,
-            easing: 'easeInOutQuad',
-            duration: 100
-        })
+        this.move_die(this.x, this.y, this.rotate)
         // Allows the dice to be saved
         $(this.dice)[0].onclick = function(e) {saveDice(e.target)}
-   }
+    }
+}
+
+// Changes the current player turn
+function change_turn() {
+    $("#action").unbind("click") // Removes the last click event for the action btn
+    $(".input").unbind("click") // Removes the click action for all of the current players paper items
+    $(".temp").remove() // Removes all .temp class elements
+    $(".input").removeClass("input") // Removes input from all of the current players paper items
+    // Resets all the dice to their original positions
+    for (var i=0; i < dieList.length; i++) {
+        dieList[i].move_die(0,0,0) // Moves the die to default position
+        dieList[i].saved = false // Unsaves the die
+    }
+    // turns
+    currTurn++
+    if (currTurn >= 3) {currTurn = 1} // If the currTurn pass the amount of players
+    rolls = 0 // Resets rolls
+    // Changes the .current for the current player
+    $(".current").removeClass("current") // Removes the old .current class
+    $(`#p${currTurn}_name`).addClass("current")
+    // Adds the input class for the current turn
+    for (var i=0; i < otherItm.length; i++) {
+        if ($(`#p${currTurn}_${otherItm[i]}`).text().length !== 0) {continue}
+        $(`#p${currTurn}_${otherItm[i]}`).addClass("input")
+    }
+    // Allows the input class to be clicked
+    $(".input").click(function(e) {add_paperVal(e.target, currTurn)})
+    // Allows the roll btn to be interactive
+    $("#action").html("Roll")
+    $("#action").click(function() {
+        roll_dice()
+        update_inputs()
+        rolls++
+        // If the player has rolled 3 times
+        if (rolls == 3) { 
+            $("#action").html("End Turn") // Changes the text to End Turn
+            $("#action").unbind("click") // Removes the click for rolling the dice
+        }
+    })
+}
+
+// When called it will add the value to the input
+function add_paperVal(elm, ply) {
+    console.log(elm)
+    if ($(elm).children().length == 0) {return}
+    $(elm).html(elm.textContent) // Sets the content to be the number
+    players[`player${ply}`]["all_total"] += parseInt(elm.textContent) // Adds to this players all_total
+    // Checks if the clicked elm is part of the first 6
+    if (items.includes(elm.id.replace(`p${ply}_`, ""))) {players[`player${ply}`]["total"] += parseInt(elm.textContent)}
+    change_turn() // Changes the turn
+    
+    // This will check the main 6 inputs if they are 
+    var list6 = 0
+    for (var i=0; i < items.length; i++) {
+        if ($(`#p${ply}_${items[i]}`).text().length !== 0) {list6++}
+    }
+    // If the first 6 inputs have values then this will check for bonus points
+    if (list6 == 6) {
+        var bonus = 0
+        $(`#p${ply}_total`).html(players[`player${ply}`]["total"])
+        // If the player has over 63 points in the first 6 inputs then they get a bonus
+        if (players[`player${ply}`]["total"] >= 63) {bonus = 35}
+        $(`#p${ply}_bonus`).html(bonus) // Sets the players bonus text
+        players[`player${ply}`]["all_total"] += bonus // Adds the bonus to the all_total value
+    }
+
+    // Checks if all inputs are entered
+    var all_inp = 0
+    for (var i=0; i < otherItm.length; i++) {
+        if ($(`#p${ply}_${items[i]}`).text().length !== 0) {all_inp++}
+    }
+    // If all inputs are enter then it will end the game and add all the items
 }
 
 // Saves the dice
@@ -99,10 +155,9 @@ function saveDice(elm) {
     else if (!die.isSaved()) {die.save_die()}
 }
 
+// Updates the diceVal with the current dice values
 function update_diceVal() {
-    for (var i=0; i <dieList.length; i++) {
-        diceVal[i] = dieList[i].get_dice_num()
-    }
+    for (var i=0; i <dieList.length; i++) {diceVal[i] = dieList[i].get_dice_num()}
     diceVal.sort(function(a, b){return a - b})
 }
 
@@ -119,16 +174,13 @@ function get_all_item(value) {
     var seen = 0
     // Goes through the list
     for (var i=0; i < diceVal.length; i++) {
-        if (diceVal[i] == value) {seen++}// Checks for the given value
+        if (diceVal[i] == value) {seen++} // Checks for the given value
     }
     return seen
 }
 
 // Returns if it is in order
 function inNumOrder(values) {
-    console.log(values, "before")
-    values.sort(function(a, b){return a - b})
-    console.log(values)
     for (var j=0; j < values.length; j++) {
         if (!(diceVal[j]+1 == diceVal[j+1])) {return false}
     }
@@ -146,60 +198,59 @@ function getTotal() {
 
 // When called it will update the paper inputs ( A lot of Checks )
 function update_inputs() {
-    var items = ["ones", "twos", "threes", "fours", "fives", "sixes"]
-    var otherItm = [items, "three_kind", "four_kind", "house", "sm_straight", "lar_straight", "chance", "yahtzee"]
+    var hasInput = false
     update_diceVal()
-    
+    $(".temp").remove() // Removes the old .temp elements
     // Goes through the items list and does checks || For single nums
     for (var i=1; i < items.length+1; i++) {
-        // Resets all innerHTMLs
-        for (var j=0; j < otherItm.length; j++) {
-            if ($(`#p${currTurn}_${otherItm[j]}`).children().length > 0) {$(`#p${currTurn}_${otherItm[j]}`).html(``)}
-        }
+        // Resets all innerHTMLs For 3 of a kind and below
         // Updaates all HTMLs
         if (diceVal.includes(i)) {
             // This will update the single digit inputs
             var times = get_all_item(i)
-            $(`#p${currTurn}_${items[i-1]}`).html(`<div>${times*i}</div>`)
+            console.log(i, times)
+            if ($(`#p${currTurn}_${items[i-1]}`).html().length == 0) {$(`#p${currTurn}_${items[i-1]}`).html(`<div class="temp">${times*i}</div>`)}
             // This will do check on the unique
             // This checks for Three of a kind
-            if (times >= 3) { $(`#p${currTurn}_three_kind`).html(`<div>${getTotal()}</div>`)}
+            if (times >= 3) { $(`#p${currTurn}_three_kind`).html(`<div class="temp">${getTotal()}</div>`);hasInput = true}
             // This checks for Four of a kind
-            if (times >= 4) { $(`#p${currTurn}_four_kind`).html(`<div>${getTotal()}</div>`)}
+            if (times >= 4) { $(`#p${currTurn}_four_kind`).html(`<div class="temp">${getTotal()}</div>`);hasInput = true}
             // This checks for YAHTZEE
-            if (times >= 5) { $(`#p${currTurn}_yahtzee`).html(`<div>${50}</div>`)}
+            if (times >= 5) { $(`#p${currTurn}_yahtzee`).html(`<div class="temp">50</div>`);hasInput = true}
         }
-        // This checks for Full House
-        var arr_dieVal = Array.from(new Set(diceVal))
-        if (arr_dieVal.length == 2) { // If the arr_dieVal is only 2 values
-            console.log(arr_dieVal)
-            console.log(get_all_item(arr_dieVal[0]), get_all_item(arr_dieVal[1]))
-            // It checks if the items have at least 2 or 3 times
-            if ((get_all_item(arr_dieVal[0]) == 3) || (get_all_item(arr_dieVal[0]) == 2)) {
-                if ((get_all_item(arr_dieVal[1]) == 3) || (get_all_item(arr_dieVal[1]) == 2)) {
-                    $(`#p${currTurn}_house`).html(`<div>${25}</div>`)
-                }
-            }   
+        
+    }
+    // This checks for Full House
+    var arr_dieVal = Array.from(new Set(diceVal))
+    if (arr_dieVal.length == 2) { // If the arr_dieVal is only 2 values
+        // It checks if the items have at least 2 or 3 times
+        if ((get_all_item(arr_dieVal[0]) == 3) || (get_all_item(arr_dieVal[0]) == 2)) {
+            if ((get_all_item(arr_dieVal[1]) == 3) || (get_all_item(arr_dieVal[1]) == 2)) {
+                $(`#p${currTurn}_house`).html(`<div class="temp">25</div>`)
+            }
+        }   
+    }
+    // This checks for small straight
+    if (arr_dieVal.length == 4) {
+        if(inNumOrder(arr_dieVal)) {$(`#p${currTurn}_sm_straight`).html(`<div class="temp">30</div>`);hasInput = true}
+    }
+    // This checks for large straight
+    if (arr_dieVal.length == 5) {$(`#p${currTurn}_lar_straight`).html(`<div class="temp">40</div>`);hasInput = true}
+    // This will allow the chance
+    if ($(`#p${currTurn}_chance`).html().length == 0) {$(`#p${currTurn}_chance`).html(`<div class="temp">${getTotal()}</div>`);hasInput = true}
+    // Checks if the player has no valid inputs
+    if (hasInput == false) {
+        for (var i=0; i < otherItm.length; i++) {
+            if ($(`#p${ply}_${items[i]}`).text().length == 0) {$(`#p${ply}_${otherItm[i]}`).html(`<div class="temp">0</div>`)}
         }
-        // This checks for small straight
-        if (arr_dieVal.length == 4) {
-            inNumOrder(arr_dieVal)
-            $(`#p${currTurn}_sm_straight`).html(`<div>${30}</div>`)
-        }
-        // This checks for large straight
-        if (arr_dieVal.length == 5) {$(`#p${currTurn}_lar_straight`).html(`<div>${40}</div>`)}
     }
 }
 
-// Makes the dice list
-for(var i=0; i < 5; i++) {dieList.push(new Dice($(`#dice${i+1}`)[0]))}
 
-// Allows the roll btn to be interactive
-$("#action").click(function() {
-    roll_dice()
-    update_inputs()
-    rolls++
-    console.log(rolls)
-    // If the player has rolled 3 times
-    if (rolls == 3) { $("#action").prop("disabled", true);}
-})
+
+// When the window loads start the game
+window.onload = function() {
+    // Makes the dice list
+    for(var i=0; i < 5; i++) {dieList.push(new Dice($(`#dice${i+1}`)[0]))}
+    change_turn()
+}
